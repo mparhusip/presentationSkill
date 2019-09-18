ce7ee07b0cc5b4b4d7cfdcb9dbab54bf32f6a9d3
Marilyn Martha Yusnita Devi Parhusip <marilyn.parhusip@gmail.com>
2019-09-18 20:32:44 +0700

    Initial Commit

---
+++ presentationSkill/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  presentationSkill
+//
+//  Created by Marilyn Martha Yusnita Devi Parhusip on 18/09/19.
+//  Copyright © 2019 Apple Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
